4b05499c000634c70dbe0b8d379bbb4b1837b694
Bilal Shahid <bilalshahid@bilals-mbp-2.myfiosgateway.com>
2022-05-23 11:44:08 -0400

    Add nft type storage view

--- cadence/__test__/test/nftcatalog.test.js
+++ cadence/__test__/test/nftcatalog.test.js
@@ -19,7 +19,10 @@ import {
   approveNFTProposal,
   rejectNFTProposal,
   removeNFTProposal,
-  withdrawNFTProposalFromCatalog
+  withdrawNFTProposalFromCatalog,
+  getNFTCollectionsForNFTType,
+  removeFromNFTCatalog,
+  updateNFTCatalogEntry
 } from '../src/nftcatalog';
 import {
   deployExampleNFT,
@@ -81,6 +84,215 @@ describe('NFT Catalog Test Suite', () => {
     expect(result).not.toBe(null);
     expect(result.contractName).toBe(nftCreationEvent.data.contract);
     expect(error).toBe(null);
+  });
+
+  it('should add to catalog with two collections in a single type', async () => {
+    await deployNFTCatalog();
+
+    let res = await deployExampleNFT();
+    const nftCreationEvent = res[0].events.find(element => element.type === 'flow.AccountContractAdded');
+
+    const [nftTypeIdentifier, _] = await getExampleNFTType();
+
+    const Alice = await getAccountAddress('Alice');
+    await setupExampleNFTCollection(Alice);
+    await shallPass(mintExampleNFT(Alice, TEST_NFT_NAME, TEST_NFT_DESCRIPTION, TEST_NFT_THUMBNAIL, [], [], []));
+
+
+    await shallPass(addToCatalogAdmin(
+      'ExampleNFT1',
+      nftCreationEvent.data.contract,
+      nftCreationEvent.data.address,
+      nftTypeIdentifier,
+      Alice,
+      0,
+      'exampleNFTCollection',
+    ));
+
+    await shallRevert(addToCatalogAdmin(
+      'ExampleNFT1',
+      nftCreationEvent.data.contract,
+      nftCreationEvent.data.address,
+      nftTypeIdentifier,
+      Alice,
+      0,
+      'exampleNFTCollection',
+    ));
+
+    await shallPass(addToCatalogAdmin(
+      'ExampleNFT2',
+      nftCreationEvent.data.contract,
+      nftCreationEvent.data.address,
+      nftTypeIdentifier,
+      Alice,
+      0,
+      'exampleNFTCollection',
+    ));
+
+    let [result, error] = await shallResolve(getNFTCollectionsForNFTType(nftTypeIdentifier));
+    expect(Object.keys(result).length).toBe(2);
+    expect(result['ExampleNFT1']).not.toBe(null);
+    expect(result['ExampleNFT2']).not.toBe(null);
+    expect(error).toBe(null);
+  });
+
+  it('should remove from catalog', async () => {
+    await deployNFTCatalog();
+
+    const Alice = await getAccountAddress('Alice');
+
+    await shallResolve(setupNFTCatalogAdminProxy(Alice));
+
+    await shallResolve(sendAdminProxyCapability(Alice));
+
+    let res = await deployExampleNFT();
+    const nftCreationEvent = res[0].events.find(element => element.type === 'flow.AccountContractAdded');
+
+    const [nftTypeIdentifier, _] = await getExampleNFTType();
+
+    await setupExampleNFTCollection(Alice);
+    await shallPass(mintExampleNFT(Alice, TEST_NFT_NAME, TEST_NFT_DESCRIPTION, TEST_NFT_THUMBNAIL, [], [], []));
+
+    await shallPass(addToCatalog(
+      Alice,
+      nftCreationEvent.data.contract,
+      nftCreationEvent.data.contract,
+      nftCreationEvent.data.address,
+      nftTypeIdentifier,
+      Alice,
+      0,
+      'exampleNFTCollection'
+    ));
+
+    await shallPass(removeFromNFTCatalog(
+      Alice,
+      nftCreationEvent.data.contract
+    ));
+
+    let [result, error] = await shallResolve(getNFTCollectionsForNFTType(nftTypeIdentifier));
+    expect(result).toBe(null);
+
+    [result, error] = await shallResolve(getNFTMetadataForCollectionName('ExampleNFT'));
+    expect(result).toBe(null);
+
+    await shallPass(addToCatalog(
+      Alice,
+      'ExampleNFT1',
+      nftCreationEvent.data.contract,
+      nftCreationEvent.data.address,
+      nftTypeIdentifier,
+      Alice,
+      0,
+      'exampleNFTCollection'
+    ));
+
+    await shallPass(addToCatalog(
+      Alice,
+      'ExampleNFT2',
+      nftCreationEvent.data.contract,
+      nftCreationEvent.data.address,
+      nftTypeIdentifier,
+      Alice,
+      0,
+      'exampleNFTCollection'
+    ));
+
+    await shallPass(removeFromNFTCatalog(
+      Alice,
+      'ExampleNFT1'
+    ));
+
+    [result, error] = await shallResolve(getNFTCollectionsForNFTType(nftTypeIdentifier));
+    expect(Object.keys(result).length).toBe(1);
+    expect(result['ExampleNFT2']).not.toBe(null);
+
+
+    [result, error] = await shallResolve(getNFTMetadataForCollectionName('ExampleNFT1'));
+    expect(result).toBe(null);
+
+    await shallPass(removeFromNFTCatalog(
+      Alice,
+      'ExampleNFT2'
+    ));
+
+    [result, error] = await shallResolve(getNFTCollectionsForNFTType(nftTypeIdentifier));
+    expect(result).toBe(null);
+
+
+    [result, error] = await shallResolve(getNFTMetadataForCollectionName('ExampleNFT1'));
+    expect(result).toBe(null);
+  });
+
+  it('should update catalog', async () => {
+    await deployNFTCatalog();
+
+    const Alice = await getAccountAddress('Alice');
+
+    await shallResolve(setupNFTCatalogAdminProxy(Alice));
+
+    await shallResolve(sendAdminProxyCapability(Alice));
+
+    let res = await deployExampleNFT();
+    const nftCreationEvent = res[0].events.find(element => element.type === 'flow.AccountContractAdded');
+
+    const [nftTypeIdentifier, _] = await getExampleNFTType();
+
+    await setupExampleNFTCollection(Alice);
+    await shallPass(mintExampleNFT(Alice, TEST_NFT_NAME, TEST_NFT_DESCRIPTION, TEST_NFT_THUMBNAIL, [], [], []));
+
+    await shallPass(addToCatalog(
+      Alice,
+      nftCreationEvent.data.contract,
+      nftCreationEvent.data.contract,
+      nftCreationEvent.data.address,
+      nftTypeIdentifier,
+      Alice,
+      0,
+      'exampleNFTCollection'
+    ));
+
+
+    await shallPass(updateNFTCatalogEntry(
+      Alice,
+      nftCreationEvent.data.contract,
+      'ExampleNFT2',
+      nftCreationEvent.data.address,
+      nftTypeIdentifier,
+      Alice,
+      0,
+      'exampleNFTCollection'
+    ));
+
+    let [result, error] = await shallResolve(getNFTCollectionsForNFTType(nftTypeIdentifier));
+    expect(Object.keys(result).length).toBe(1);
+    expect(result[nftCreationEvent.data.contract]).not.toBe(null);
+
+    [result, error] = await shallResolve(getNFTMetadataForCollectionName(nftCreationEvent.data.contract));
+    expect(result.contractName).toBe('ExampleNFT2');
+
+    const tempNewIdentifer = nftTypeIdentifier.replace('.NFT', '.Collection'); // hacky way to test another type
+
+    await shallPass(updateNFTCatalogEntry(
+      Alice,
+      nftCreationEvent.data.contract,
+      'ExampleNFT2',
+      nftCreationEvent.data.address,
+      tempNewIdentifer,
+      Alice,
+      0,
+      'exampleNFTCollection'
+    ));
+
+    [result, error] = await shallResolve(getNFTCollectionsForNFTType(nftTypeIdentifier));
+    expect(result).toBe(null);
+
+    [result, error] = await shallResolve(getNFTCollectionsForNFTType(tempNewIdentifer));
+    expect(Object.keys(result).length).toBe(1);
+    expect(result[nftCreationEvent.data.contract]).not.toBe(null);
+
+    [result, error] = await shallResolve(getNFTMetadataForCollectionName(nftCreationEvent.data.contract));
+    expect(result.contractName).toBe('ExampleNFT2');
+    expect(result.nftType.typeID).toBe(tempNewIdentifer);
   });
 
   it('non-admin accounts should be able to receive admin capability', async () => {
---
+++ cadence/transactions/remove_from_nft_catalog.cdc
@@ -0,0 +1,17 @@
+import MetadataViews from "../contracts/MetadataViews.cdc"
+import NFTCatalog from "../contracts/NFTCatalog.cdc"
+import NFTCatalogAdmin from "../contracts/NFTCatalogAdmin.cdc"
+
+transaction(
+  collectionName : String
+) {
+  let adminProxyResource : &NFTCatalogAdmin.AdminProxy
+
+  prepare(acct: AuthAccount) {
+    self.adminProxyResource = acct.borrow<&NFTCatalogAdmin.AdminProxy>(from : NFTCatalogAdmin.AdminProxyStoragePath)!
+  }
+
+  execute {   
+    self.adminProxyResource.getCapability()!.borrow()!.removeCatalogEntry(collectionName : collectionName)
+  }
+}
