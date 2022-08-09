53deb52510a50de2e57430af89cf6fc8188149b7
Amit Ishairzay <amit.ishairzay@gmail.com>
2022-08-09 12:50:33 -0400

    Show proper public and private types on proposal page and allow for non-0x addresses.

--- ui/apps/nft-portal/src/app/components/shared/views/collection-data-view.tsx
+++ ui/apps/nft-portal/src/app/components/shared/views/collection-data-view.tsx
@@ -10,14 +10,16 @@ function convertToReadableType(type: string) {
 }
 
 export function CollectionDataView({ view, withRawView }: GenericViewProps) {
+  const publicTypeID = view.publicLinkedType.typeID ? view.publicLinkedType.typeID : view.publicLinkedType.type.typeID
+  const privateTypeID = view.privateLinkedType.typeID ? view.privateLinkedType.typeID : view.privateLinkedType.type.typeID
   return (
     <>
       <div className="w-full overflow-x-auto text-md">
         <div><b>Storage Path:</b> /storage/{view.storagePath.identifier}</div>
         <div><b>Public Path:</b> /public/{view.storagePath.identifier}</div>
         <div><b>Private Path:</b> /private/{view.storagePath.identifier}</div>
-        <div><b>Public Type:</b> {convertToReadableType(view.publicLinkedType.type.typeID)}</div>
-        <div><b>Private Type:</b> {convertToReadableType(view.privateLinkedType.type.typeID)}</div>
+        <div><b>Public Type:</b> {convertToReadableType(publicTypeID)}</div>
+        <div><b>Private Type:</b> {convertToReadableType(privateTypeID)}</div>
       </div>
       {
         withRawView && (
