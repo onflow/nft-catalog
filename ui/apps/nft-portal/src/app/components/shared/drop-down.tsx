e99bd113223061c5d454f569a867ad86604ed80f
bilal <bshahid331@gmail.com>
2022-09-27 14:25:15 -0400

    Make site more responsive (#82)
    * make navbar responsive
    
    * Make catalog select related pages responsive
    
    * fix select on proposal pages
    
    * fix overflow

--- ui/apps/nft-portal/src/app/components/shared/drop-down.tsx
+++ ui/apps/nft-portal/src/app/components/shared/drop-down.tsx
@@ -21,13 +21,13 @@ export function DropDown<Type extends OptionValue>({ label, value, onChange, opt
   }
 
   return (
-    <div className="md:flex md:items-center my-6">
-      <div className="md:w-1/3">
-        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+    <div className="lg:flex lg:items-center my-6">
+      <div className="lg:w-1/3">
+        <label className="block text-gray-500 font-bold lg:text-right mb-1 md:mb-0 pr-4">
           {label}
         </label>
       </div>
-      <div className="md:w-2/3">
+      <div className="lg:w-2/3">
         <select value={value} onChange={handleOnChange} className={`w-11/12 form-select shadow-sm rounded border-gray-300 bg-gray-50 text-sm cursor-pointer focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500`}>
           {options.map(option => (
             <option key={option.value} value={option.value}>
