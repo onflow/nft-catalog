b4116cfc580e60adb824a2b6b0af1fa83ae64661
bilal <bshahid331@gmail.com>
2022-09-30 14:30:05 -0400

    Support ipfs urls in display view (#87)

--- ui/apps/nft-portal/src/app/components/shared/views/display-view.tsx
+++ ui/apps/nft-portal/src/app/components/shared/views/display-view.tsx
@@ -5,13 +5,17 @@ type GenericViewProps = {
 }
 
 export function DisplayView({ view }: GenericViewProps) {
+  let { thumbnail } = view
+  if (thumbnail.startsWith('ipfs://')) {
+    thumbnail = "https://ipfs.io/ipfs/" + thumbnail.substring(7)
+  }
   return (
     <>
       <div className="text-2xl">{view.name}</div>
       <div className="text-md mt-2">
         {view.description}
       </div>
-      <img src={view.thumbnail} width="50" height="50"></img>
+      <img src={thumbnail} width="50" height="50"></img>
       <hr className="my-2" />
       <GenericViewToggle view={view} />
     </>
