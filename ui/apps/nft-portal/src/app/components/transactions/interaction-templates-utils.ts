5853c0cd9e2116a2b632ed7d872e7f742610572f
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2022-11-16 14:17:09 -0800

    Adds argument messages

--- ui/apps/nft-portal/src/app/components/transactions/interaction-templates-utils.ts
+++ ui/apps/nft-portal/src/app/components/transactions/interaction-templates-utils.ts
@@ -29,7 +29,109 @@ export const TRANSACTION_TYPES: any = {
         ],
       }),
     ],
-    args: ({ projectName }: { projectName: string }) => [],
+    args: ({ projectName }: { projectName: string }) => [
+      FLIX_Generators.arg({
+        tag: 'saleItemID',
+        type: 'UInt64',
+        index: 0,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'ID of the NFT that is put on sale by the seller',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'saleItemPrice',
+        type: 'UInt64',
+        index: 1,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Amount of tokens (FT) buyer needs to pay for the purchase of listed NFT',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'customID',
+        type: 'String?',
+        index: 2,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Optional string to represent identifier of the dapp',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'commissionAmount',
+        type: 'UFix64',
+        index: 3,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Commission amount that will be taken away by the purchase facilitator',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'expiry',
+        type: 'UFix64',
+        index: 4,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Unix timestamp at which created listing become expired',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'marketplacesAddress',
+        type: '[Address]',
+        index: 5,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'List of addresses that are allowed to get the commission',
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
   },
   StorefrontBuyItem: {
     type: 'transaction',
@@ -44,7 +146,57 @@ export const TRANSACTION_TYPES: any = {
         ],
       }),
     ],
-    args: ({ projectName }: { projectName: string }) => [],
+    args: ({ projectName }: { projectName: string }) => [
+      FLIX_Generators.arg({
+        tag: 'listingResourceID',
+        type: 'UInt64',
+        index: 0,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'ID of the Storefront listing resource',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'storefrontAddress',
+        type: 'Address',
+        index: 1,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'The address that owns the storefront listing',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'commissionRecipient',
+        type: 'Address?',
+        index: 2,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Optional recipient for transaction commission if comission exists',
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
   },
   StorefrontRemoveItem: {
     type: 'transaction',
@@ -59,7 +211,24 @@ export const TRANSACTION_TYPES: any = {
         ],
       }),
     ],
-    args: ({ projectName }: { projectName: string }) => [],
+    args: ({ projectName }: { projectName: string }) => [
+      FLIX_Generators.arg({
+        tag: 'listingResourceID',
+        type: 'UInt64',
+        index: 0,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'ID of the Storefront listing resource',
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
   },
   DapperBuyNFTMarketplace: {
     messages: ({ projectName }: { projectName: string }) => [
@@ -73,7 +242,73 @@ export const TRANSACTION_TYPES: any = {
         ],
       }),
     ],
-    args: ({ projectName }: { projectName: string }) => [],
+    args: ({ projectName }: { projectName: string }) => [
+      FLIX_Generators.arg({
+        tag: 'storefrontAddress',
+        type: 'Address',
+        index: 0,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'The address that owns the storefront listing',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'listingResourceID',
+        type: 'UInt64',
+        index: 1,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'ID of the Storefront listing resource',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'expectedPrice',
+        type: 'UFix64',
+        index: 2,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'How much you expect to pay for the NFT',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'commissionRecipient',
+        type: 'Address?',
+        index: 3,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Optional recipient for transaction commission if comission exists',
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
   },
   DapperCreateListing: {
     type: 'transaction',
@@ -88,7 +323,109 @@ export const TRANSACTION_TYPES: any = {
         ],
       }),
     ],
-    args: ({ projectName }: { projectName: string }) => [],
+    args: ({ projectName }: { projectName: string }) => [
+      FLIX_Generators.arg({
+        tag: 'saleItemID',
+        type: 'UInt64',
+        index: 0,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'ID of the NFT that is put on sale by the seller',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'saleItemPrice',
+        type: 'UFix64',
+        index: 1,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Amount of tokens (FT) buyer needs to pay for the purchase of listed NFT',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'commissionAmount',
+        type: 'UFix64',
+        index: 2,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Commission amount that will be taken away by the purchase facilitator',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'marketplacesAddress',
+        type: '[Address]',
+        index: 3,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'List of addresses that are allowed to get the commission',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'expiry',
+        type: 'UInt64',
+        index: 4,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Unix timestamp at which created listing become expired',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'customID',
+        type: 'String?',
+        index: 5,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Optional string to represent identifier of the dapp',
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
   },
   DapperBuyNFTDirect: {
     type: 'transaction',
@@ -103,7 +440,90 @@ export const TRANSACTION_TYPES: any = {
         ],
       }),
     ],
-    args: ({ projectName }: { projectName: string }) => [],
+    args: ({ projectName }: { projectName: string }) => [
+      FLIX_Generators.arg({
+        tag: 'merchantAccountAddress',
+        type: 'Address',
+        index: 0,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'The merchant account address provided by Dapper Labs',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'storefrontAddress',
+        type: 'Address',
+        index: 1,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'The address that owns the storefront listing',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'listingResourceID',
+        type: 'UInt64',
+        index: 2,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'ID of the Storefront listing resource',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'expectedPrice',
+        type: 'UFix64',
+        index: 3,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation: 'How much you expect to pay for the NFT',
+              }),
+            ],
+          }),
+        ],
+      }),
+      FLIX_Generators.arg({
+        tag: 'commissionRecipient',
+        type: 'Address?',
+        index: 4,
+        messages: [
+          FLIX_Generators.message({
+            tag: 'title',
+            translations: [
+              FLIX_Generators.messageTranslation({
+                bcp47tag: 'en-US',
+                translation:
+                  'Optional recipient for transaction commission if comission exists',
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
   },
   DapperGetPrimaryListingMetadata: {
     type: 'script',
--- ui/apps/nft-portal/src/app/components/transactions/transaction-audit-status.tsx
+++ ui/apps/nft-portal/src/app/components/transactions/transaction-audit-status.tsx
@@ -1,23 +1,13 @@
-import { Box } from "../shared/box"
-
 export function TransactionAuditStatus({
   isAudited,
   auditorName,
-  // auditorImg,
 }: { isAudited: boolean, auditorName: string }) {
   return (
     <div className="w-1/4 p-4 rounded-xl border-2 mb-4 mr-4">
       <div className="text-2xl">{auditorName}</div>
       <div className="text-md mt-2 text-md mt-2 p-4 border-x-primary-purple border-l-4">
-        {/* { isAudited ? 
-          <div className="w-24 h-24 rounded-full bg-primary-red" />
-          :
-          <div className="w-24 h-24 rounded-full bg-primary-green" />
-        } */}
         {isAudited ? "Audited" : "Not Audited"}
       </div>
-      {/* <img src={auditorImg} width="50" height="50"></img> */}
-      {/* <hr className="my-2" /> */}
     </div>
   )
 }
