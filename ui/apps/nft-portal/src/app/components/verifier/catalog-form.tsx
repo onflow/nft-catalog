1711b6f94fca457a7495afb88bbed6cc6bbe2977
Pratik Patel <prpatel05@gmail.com>
2023-02-06 13:10:01 -0500

    Don't Allow . For Collection ID.

--- ui/apps/nft-portal/src/app/components/verifier/catalog-form.tsx
+++ ui/apps/nft-portal/src/app/components/verifier/catalog-form.tsx
@@ -1,105 +1,168 @@
 import { TextInput } from '../shared/text-input';
 import { useState, useEffect } from 'react';
-import { useParams, useNavigate } from "react-router-dom";
-import { Alert } from "../shared/alert"
-import { Spinner } from "../shared/spinner"
-import { proposeNFTToCatalog, getNFTMetadataForCollectionIdentifier } from "../../../flow/utils"
+import { useParams, useNavigate } from 'react-router-dom';
+import { Alert } from '../shared/alert';
+import { Spinner } from '../shared/spinner';
+import {
+  proposeNFTToCatalog,
+  getNFTMetadataForCollectionIdentifier,
+} from '../../../flow/utils';
 import { useDebounce } from '../../../app/hooks/use-debounce';
-import { getNFTsInAccount } from "../../../flow/utils"
-import * as fcl from "@onflow/fcl";
+import { getNFTsInAccount } from '../../../flow/utils';
+import * as fcl from '@onflow/fcl';
 import { VerifierInfoBox } from './verifier-info-box';
 
 type CatalogProps = {
-  sampleAddress: string | null
-  storagePath: string | null,
-  nftID: string | null
-}
+  sampleAddress: string | null;
+  storagePath: string | null;
+  nftID: string | null;
+};
 
-export function CatalogForm({ sampleAddress, storagePath, nftID }: CatalogProps) {
-  const navigate = useNavigate()
-  const [collectionIdentifier, setCollectionIdentifier] = useState<string>("")
-  const debouncedCollectionIdentifier: string = useDebounce<string>(collectionIdentifier, 500);
-  const [message, setMessage] = useState<string>("")
-  const [emailAddress, setEmailAddress] = useState<string>("")
-  const { selectedNetwork, selectedAddress, selectedContract } = useParams<any>()
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error, setError] = useState<string | null>(null)
+export function CatalogForm({
+  sampleAddress,
+  storagePath,
+  nftID,
+}: CatalogProps) {
+  const navigate = useNavigate();
+  const [collectionIdentifier, setCollectionIdentifier] = useState<string>('');
+  const debouncedCollectionIdentifier: string = useDebounce<string>(
+    collectionIdentifier,
+    500
+  );
+  const [message, setMessage] = useState<string>('');
+  const [emailAddress, setEmailAddress] = useState<string>('');
+  const { selectedNetwork, selectedAddress, selectedContract } =
+    useParams<any>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
   const [warning, setWarning] = useState<string | null>(null);
-  const [user, setUser] = useState({ loggedIn: null })
+  const [user, setUser] = useState({ loggedIn: null });
 
-  useEffect(() => fcl.currentUser().subscribe(setUser), [])
+  useEffect(() => fcl.currentUser().subscribe(setUser), []);
 
   useEffect(() => {
-    if (!sampleAddress || !storagePath || !nftID) { return }
+    if (!sampleAddress || !storagePath || !nftID) {
+      return;
+    }
     const getNfts = async () => {
       const allNFTs = await getNFTsInAccount(sampleAddress, storagePath);
       if (!collectionIdentifier || collectionIdentifier === '') {
         const selectedNft = allNFTs.find((nft: { Id: string }) => {
-          return nft.Id === nftID
-        })
+          return nft.Id === nftID;
+        });
         if (selectedNft && selectedNft.NFTCollectionDisplay) {
           // Get rid of spaces if they exist..
-          setCollectionIdentifier(selectedNft.NFTCollectionDisplay.collectionName.replace(/\s+/g, ''))
+          setCollectionIdentifier(
+            selectedNft.NFTCollectionDisplay.collectionName.replace(/\s+/g, '')
+          );
         }
       }
-    }
-    getNfts()
-  }, [])
+    };
+    getNfts();
+  }, []);
 
   useEffect(() => {
     const metadataInformation = async () => {
-      const res = await getNFTMetadataForCollectionIdentifier(debouncedCollectionIdentifier);
+      const res = await getNFTMetadataForCollectionIdentifier(
+        debouncedCollectionIdentifier
+      );
       if (res != null) {
-        setError(null)
-        setWarning("An entry with this collection identifier already exists in the catalog. This proposal will be proposing an update.")
+        setError(null);
+        setWarning(
+          'An entry with this collection identifier already exists in the catalog. This proposal will be proposing an update.'
+        );
       } else {
         setWarning(null);
       }
-    }
+    };
     if (debouncedCollectionIdentifier !== '') {
-      metadataInformation()
+      metadataInformation();
     }
-  }, [debouncedCollectionIdentifier])
+  }, [debouncedCollectionIdentifier]);
 
   function validateIdentifier(collectionIdentifier: string): boolean {
-    return !/\s/.test(collectionIdentifier) && !/-/.test(collectionIdentifier)
+    return (
+      !/\s/.test(collectionIdentifier) &&
+      !/-/.test(collectionIdentifier) &&
+      !/\./.test(collectionIdentifier)
+    );
   }
 
   return (
     <>
-      {warning && <><Alert status="warning" title={warning} body="" /><br /></>}
-      {error && <><Alert status="error" title={error} body="" /><br /></>}
-      <form className="my-12 w-7/12" onSubmit={async (e) => {
-        e.preventDefault();
-        setError(null);
-        setWarning(null);
-        if (collectionIdentifier === '' || collectionIdentifier == null || message === '' || message == null) {
-          setError("Missing Data");
-          return;
-        }
-        if (!storagePath || !sampleAddress || !selectedAddress || !selectedContract || !nftID) {
-          setError("Missing Data");
-          return;
-        }
-        if (!validateIdentifier(collectionIdentifier)) {
-          setError("Invalid identifier, please make sure you don't include any spaces or dashes.")
-          return;
-        }
-        if (!user.loggedIn) {
-          await fcl.logIn()
-        }
-        setLoading(true);
-        const proposalMessage = message + " ( This proposal was made via: " + window.location.href + " )"
-        try {
-          await proposeNFTToCatalog(collectionIdentifier, sampleAddress, nftID, storagePath, selectedContract, selectedAddress, proposalMessage);
+      {warning && (
+        <>
+          <Alert status="warning" title={warning} body="" />
+          <br />
+        </>
+      )}
+      {error && (
+        <>
+          <Alert status="error" title={error} body="" />
+          <br />
+        </>
+      )}
+      <form
+        className="my-12 w-7/12"
+        onSubmit={async (e) => {
+          e.preventDefault();
           setError(null);
-          navigate(`/submitted`);
-        } catch (e: any) {
-          setError(e.toString());
-        }
-        setLoading(false);
-      }}>
-        <p className='text-sm mb-4 font-bold'>Enter a unique identifier (title) to describe this collection</p>
+          setWarning(null);
+          if (
+            collectionIdentifier === '' ||
+            collectionIdentifier == null ||
+            message === '' ||
+            message == null
+          ) {
+            setError('Missing Data');
+            return;
+          }
+          if (
+            !storagePath ||
+            !sampleAddress ||
+            !selectedAddress ||
+            !selectedContract ||
+            !nftID
+          ) {
+            setError('Missing Data');
+            return;
+          }
+          if (!validateIdentifier(collectionIdentifier)) {
+            setError(
+              "Invalid identifier, please make sure you don't include any spaces or dashes."
+            );
+            return;
+          }
+          if (!user.loggedIn) {
+            await fcl.logIn();
+          }
+          setLoading(true);
+          const proposalMessage =
+            message +
+            ' ( This proposal was made via: ' +
+            window.location.href +
+            ' )';
+          try {
+            await proposeNFTToCatalog(
+              collectionIdentifier,
+              sampleAddress,
+              nftID,
+              storagePath,
+              selectedContract,
+              selectedAddress,
+              proposalMessage
+            );
+            setError(null);
+            navigate(`/submitted`);
+          } catch (e: any) {
+            setError(e.toString());
+          }
+          setLoading(false);
+        }}
+      >
+        <p className="text-sm mb-4 font-bold">
+          Enter a unique identifier (title) to describe this collection
+        </p>
         <TextInput
           value={collectionIdentifier}
           updateValue={setCollectionIdentifier}
@@ -107,7 +170,7 @@ export function CatalogForm({ sampleAddress, storagePath, nftID }: CatalogProps)
         />
         <br />
         <br />
-        <p className='text-sm mb-4 font-bold'>Add a description</p>
+        <p className="text-sm mb-4 font-bold">Add a description</p>
         <TextInput
           value={message}
           updateValue={setMessage}
@@ -122,13 +185,17 @@ export function CatalogForm({ sampleAddress, storagePath, nftID }: CatalogProps)
           placeholder=""
         />
     <br />*/}
-        {loading ? <Spinner /> : <input
-          type="submit"
-          value={"Submit for review"}
-          className="cursor-pointer bg-black hover:bg-gray-100 text-white text-sm hover:text-black py-4 px-6 rounded-md"
-        />}
+        {loading ? (
+          <Spinner />
+        ) : (
+          <input
+            type="submit"
+            value={'Submit for review'}
+            className="cursor-pointer bg-black hover:bg-gray-100 text-white text-sm hover:text-black py-4 px-6 rounded-md"
+          />
+        )}
       </form>
       <VerifierInfoBox />
     </>
-  )
+  );
 }
