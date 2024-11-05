80c331ea5a6800379468f24001d920f8a4e93d93
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2024-11-05 14:34:39 -0500

    fix style

--- ui/apps/nft-portal/src/app/components/shared/deprecation-notice.tsx
+++ ui/apps/nft-portal/src/app/components/shared/deprecation-notice.tsx
@@ -3,13 +3,13 @@ import {FaExclamationTriangle} from 'react-icons/fa'
 
 export const DeprecationNotice = () => {
   return (
-    <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4" role="alert">
+    <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 text-center flex justify-center" role="alert">
       <div className="flex">
-        <div className="flex-shrink-0">
-          <FaExclamationTriangle />
+        <div className="flex flex-shrink-0 items-center">
+          <FaExclamationTriangle size={32} />
         </div>
         <div className="ml-3">
-          <p className="text-sm font-medium text-yellow-700">
+          <p className="text-md font-medium text-yellow-700">
             Deprecation Notice
           </p>
           <p className="mt-1 text-sm text-yellow-700">
